74b39e386accc92a6e366d7ec3c5097a5554df81
vawvaw <valentin@wiedekind1.de>
2023-02-24 21:50:38 +0100

    Hardcode socket path for gpg-agent forwarding
    I need to forward the socket under /run/user in an seemingly random
    named directory.

--- home/vawvaw/features/cli/ssh.nix
+++ home/vawvaw/features/cli/ssh.nix
@@ -1,6 +1,28 @@
 { outputs, lib, ... }:
-{
+let
+  hostnames = builtins.attrNames outputs.nixosConfigurations;
+in {
   programs.ssh = {
     enable = true;
+    matchBlocks = {
+      net = {
+        host = builtins.concatStringsSep " " hostnames;
+        forwardAgent = true;
+        remoteForwards = [
+          {
+            bind.address = ''/run/user/1000/gnupg/d.db6k4od13tuchts5jdmgus9t/S.gpg-agent'';
+            host.address = ''/run/user/1000/gnupg/d.db6k4od13tuchts5jdmgus9t/S.gpg-agent.extra'';
+          }
+          {
+            bind.address = ''/run/user/1000/gnupg/d.db6k4od13tuchts5jdmgus9t/S.gpg-agent.ssh'';
+            host.address = ''/run/user/1000/gnupg/d.db6k4od13tuchts5jdmgus9t/S.gpg-agent.ssh'';
+          }
+        ];
+      };
+      trusted = lib.hm.dag.entryBefore [ "net" ] {
+        host = "home.vaw-valentin.de";
+        forwardAgent = true;
+      };
+    };
   };
 }
