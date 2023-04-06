5897ec306bd3528a190ef457dc0230e055f0d6af
vawvaw <valentin@wiedekind1.de>
2023-04-06 20:05:29 +0200

    laptop: Remove sudo password for backlight

--- hosts/common/optional/desktop/default.nix
+++ hosts/common/optional/desktop/default.nix
@@ -9,4 +9,10 @@
   programs.dconf.enable = true;
   # gnome pinentry support
   services.dbus.packages = [ pkgs.gcr ];
+
+  # enable backlight setting without password
+  security.sudo.extraRules = [{
+      groups = [ "wheel" ];
+      commands = [ { command = "${pkgs.light}/bin/light"; options = [ "NOPASSWD" ]; } ];
+  }];
 }
