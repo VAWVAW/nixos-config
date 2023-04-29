603ea2bcb0d1e97125db4a940b71ee00eb6b1e19
vawvaw <valentin@wiedekind1.de>
2023-04-29 19:40:09 +0200

    server: Add phone user

--- hosts/vserver/config/default.nix
+++ hosts/vserver/config/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./matrix.nix
     ./nginx.nix
+    ./phone.nix
   ];
 }
---
+++ hosts/vserver/config/phone.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+{
+  users.users.phone = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      ''command="${pkgs.rrsync}/bin/rrsync /home/phone/" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3hHgB96sdq0zQZXcC5cLbOeEwYV2sm6Lpo3DrxbpeN''
+    ];
+  };
+}
