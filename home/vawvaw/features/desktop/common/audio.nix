e38e622e2f059b309bf43626fc5ad550a65e9c52
vawvaw <valentin@wiedekind1.de>
2023-04-29 18:54:09 +0200

    vawvaw: Add pavucontrol as program

--- home/vawvaw/features/desktop/common/audio.nix
+++ home/vawvaw/features/desktop/common/audio.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs;[
     wireplumber
     noisetorch
+    pavucontrol
     (writeScriptBin "set-volume" "${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ $1")
   ];
 }
