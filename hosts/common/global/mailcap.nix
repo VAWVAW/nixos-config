96658acce4dbad74d8078304e606e3603d6108d5
vawvaw <valentin@wiedekind1.de>
2023-05-29 23:45:07 +0200

    mutt: Open html mail with w3m

--- hosts/common/global/mailcap.nix
+++ hosts/common/global/mailcap.nix
@@ -4,6 +4,6 @@
     application/*; ${pkgs.xdg-utils}/bin/xdg-open %s
     audio/*; ${pkgs.xdg-utils}/bin/xdg-open %s
     image/*; ${pkgs.xdg-utils}/bin/xdg-open %s
-    text/html; ${pkgs.xdg-utils}/bin/xdg-open %s
+    text/html; ${pkgs.w3m}/bin/w3m -I %{charset} -T text/html; copiousoutput;
   '';
 }
