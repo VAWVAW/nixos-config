811385e02897b6d23bc26ba4f28d574a085d5cd9
vawvaw <valentin@wiedekind1.de>
2023-08-21 17:01:55 +0200

    athena: Use local copy of nixos config

--- home/vawvaw/athena.nix
+++ home/vawvaw/athena.nix
@@ -7,14 +7,14 @@
   programs.zsh.promptColor = "yellow";
   home.shellAliases = {
     nswitch =
-      "sudo umount /boot && sudo mount /boot -o rw && sudo nixos-rebuild switch --flake github:vawvaw/nixos-config# --refresh && sudo umount /boot && sudo mount /boot";
+      "sudo umount /boot && sudo mount /boot -o rw && sudo nixos-rebuild switch --flake /var/lib/syncthing/data/Documents/nixos-config# --refresh && sudo umount /boot && sudo mount /boot";
     nboot =
-      "sudo umount /boot && sudo mount /boot -o rw && sudo nixos-rebuild boot --flake github:vawvaw/nixos-config# --refresh && sudo umount /boot && sudo mount /boot";
+      "sudo umount /boot && sudo mount /boot -o rw && sudo nixos-rebuild boot --flake /var/lib/syncthing/data/Documents/nixos-config# --refresh && sudo umount /boot && sudo mount /boot";
     nbuild =
-      "nixos-rebuild build --flake github:vawvaw/nixos-config# --refresh";
+      "nixos-rebuild build --flake /var/lib/syncthing/data/Documents/nixos-config# --refresh";
     ntest =
-      "sudo nixos-rebuild test --flake github:vawvaw/nixos-config# --refresh";
-    hswitch = "home-manager switch --flake github:vawvaw/nixos-config";
+      "sudo nixos-rebuild test --flake /var/lib/syncthing/data/Documents/nixos-config# --refresh";
+    hswitch = "home-manager switch --flake /var/lib/syncthing/data/Documents/nixos-config";
 
   };
 }
