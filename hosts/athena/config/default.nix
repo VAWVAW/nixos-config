bccfd7e12579ed0bc1b85c873520fea6950e9561
vawvaw <valentin@wiedekind1.de>
2023-08-21 16:22:51 +0200

    athena: Add syncthing

---
+++ hosts/athena/config/default.nix
@@ -0,0 +1,1 @@
+{ imports = [ ./syncthing.nix ]; }
---
+++ hosts/athena/config/syncthing.nix
@@ -0,0 +1,15 @@
+{ config, ... }: {
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    dataDir = "/var/lib/syncthing/data";
+    configDir = "/var/lib/syncthing/config";
+
+    settings.options = { localAnnounceEnabled = false; };
+  };
+
+  environment.persistence."/backed_up".directories =
+    [ config.services.syncthing.dataDir ];
+  environment.persistence."/persist".directories =
+    [ config.services.syncthing.configDir ];
+}
