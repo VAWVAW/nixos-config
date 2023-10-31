51ad9802a5d2d85a74d7a763904e259069b348d4
vawvaw <valentin@wiedekind1.de>
2023-10-31 09:26:13 +0100

    nvim: Fix treesitter warning

--- home/vawvaw/features/cli/nvim/treesitter.lua
+++ home/vawvaw/features/cli/nvim/treesitter.lua
@@ -1,4 +1,11 @@
 require("nvim-treesitter.configs").setup {
+
+  ensure_installed = {},
+  ignore_install = {},
+  sync_install = false,
+  auto_install = false,
+  modules = {},
+
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
