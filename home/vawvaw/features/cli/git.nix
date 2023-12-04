e9e35f8ca41e7a896ceca9b93f9201908f18ad3e
vawvaw <valentin@wiedekind1.de>
2023-12-04 12:20:15 +0100

    home/git: Modify graph alias

--- home/vawvaw/features/cli/git.nix
+++ home/vawvaw/features/cli/git.nix
@@ -6,7 +6,11 @@
     aliases = {
       a = "add -A";
       ca = "commit -a";
-      graph = "log --decorate --oneline --graph";
+      graph = "graph-branch --all";
+      graph-branch =
+        "log --graph --abbrev-commit --decorate=short --format=format:'%C(bold cyan)%h%C(reset) - %C(yellow)(%ar)%C(reset) %C(brightwhite)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+      graph-long =
+        "log --graph --abbrev-commit --decorate=short --all --format=format:'%C(bold cyan)%h%C(reset) - %C(bold green)%aD%C(reset) %C(yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(brightwhite)%s%C(reset) %C(dim white)- %an%C(reset)'";
     };
     signing = {
       key = "508F0546A3908E3FE6732B8F9BEFF32F6EF32DA8";
