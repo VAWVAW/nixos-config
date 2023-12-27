b78482f42ab67ee823ce06bd33b633f89a729bec
vawvaw <valentin@wiedekind1.de>
2023-12-27 15:47:41 +0100

    zeus: Move ssl certs to right directory

--- hosts/zeus/config/default.nix
+++ hosts/zeus/config/default.nix
@@ -1,4 +1,1 @@
-{
-  imports = [ ./tlp.nix ];
-  environment.etc."ca-fu-berlin.pem".source = ./ca-fu-berlin.pem;
-}
+{ imports = [ ./ssl-certs.nix ./tlp.nix ]; }
---
+++ hosts/zeus/config/ssl-certs.nix
@@ -0,0 +1,3 @@
+{
+  environment.etc."ssl/certs/fu-berlin.pem".source = ./ca-fu-berlin.pem;
+}
