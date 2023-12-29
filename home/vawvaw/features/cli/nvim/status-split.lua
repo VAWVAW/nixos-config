5f74a8974e37c07f87790ee12989c4f6502fefb8
vawvaw <valentin@wiedekind1.de>
2023-12-29 21:09:14 +0100

    home/nvim: Use SymbolsOutline for status split

--- home/vawvaw/features/cli/nvim/status-split.lua
+++ home/vawvaw/features/cli/nvim/status-split.lua
@@ -1,4 +1,4 @@
-local aerial = require("aerial")
+local outline = require("symbols-outline")
 local nvim_tree = require("nvim-tree.api")
 
 local function open_status_split()
@@ -6,16 +6,14 @@ local function open_status_split()
     return
   end
   STATUS_SPLIT = true
-  aerial.open()
-  vim.cmd("above split")
-  nvim_tree.tree.open({ current_window = true })
-  vim.api.nvim_win_set_width(0, 30)
+  nvim_tree.tree.close()
+  outline.open_outline()
 end
 
 local function close_status_split()
   STATUS_SPLIT = false
   nvim_tree.tree.close()
-  aerial.close()
+  outline.close_outline()
 end
 
 local function toggle_status_split()
@@ -26,30 +24,35 @@ local function toggle_status_split()
   end
 end
 
-local function focus_status_split()
-  if STATUS_SPLIT ~= true then
-    open_status_split()
-  end
-
-  nvim_tree.tree.focus()
-end
-
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>e", focus_status_split, opts)
+vim.keymap.set("n", "<leader>e", nvim_tree.tree.focus, opts)
 vim.keymap.set("n", "<leader>w", toggle_status_split, opts)
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local FormatOptions = augroup("FormatOptions", { clear = true })
+autocmd(
+  "FileType", {
+    group = FormatOptions,
+    pattern = "Outline",
+    desc = "open nvim-tree",
+    callback = function()
+      vim.cmd("setlocal signcolumn=no")
+      vim.cmd("above split")
+      nvim_tree.tree.open({ current_window = true })
+      vim.api.nvim_win_set_width(0, 30)
+    end
+  }
+)
 autocmd("VimEnter", {
   group = FormatOptions,
   pattern = "*",
   desc = "Open status split",
   callback = function()
-    open_status_split()
+    nvim_tree.tree.focus()
     vim.cmd("wincmd l")
   end,
 })
