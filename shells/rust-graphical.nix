360fde4b60023f2211def5adbebc5e831e384a50
vawvaw <valentin@wiedekind1.de>
2024-01-21 11:19:32 +0100

    shells/rust-graphical: Add indicated dependencies

--- shells/rust-graphical.nix
+++ shells/rust-graphical.nix
@@ -2,13 +2,20 @@
 pkgs.mkShell rec {
   buildInputs = (import ./rust.nix { }).buildInputs ++ (with pkgs; [
     libxkbcommon
+    expat
+    freetype
+    freetype.dev
     libGL
+
     vulkan-loader
+    vulkan-tools
+    vulkan-headers
 
     libz
 
     # WINIT_UNIX_BACKEND=wayland
     wayland
+    wayland-protocols
 
     # WINIT_UNIX_BACKEND=x11
     xorg.libXcursor
@@ -16,6 +23,7 @@ pkgs.mkShell rec {
     xorg.libXi
     xorg.libX11
 
+    gperf
   ]);
 
   LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
