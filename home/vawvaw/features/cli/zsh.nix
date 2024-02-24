999b696ce482b180b37a30ff4101515d271d86f6
vawvaw <valentin@wiedekind1.de>
2024-02-24 15:48:20 +0100

    home/zsh: Add git branch to prompt

--- home/vawvaw/features/cli/zsh.nix
+++ home/vawvaw/features/cli/zsh.nix
@@ -40,6 +40,15 @@
       zstyle ':completion:*' verbose true
       zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
+      # enable vcs integration
+      autoload -Uz vcs_info
+      zstyle ':vcs_info:*' enable git
+      zstyle ':vcs_info:*' formats '%F{14}%b%%b%F{green}]-[%B%F{magenta}%r%f/%S'
+      zstyle ':vcs_info:git:*' formats '%F{magenta}%r%f/%S'
+      zstyle ':vcs_info:*' actionformats '%F{14}%b%%b%F{green}]-[%B%F{red}%a%%b%F{green}]-[%B%F{magenta}%r%f/%S'
+      zstyle ':vcs_info:git:*' actionformats '%F{red}%a%%b%F{green}]-[%B%F{magenta}%r%f/%S'
+      zstyle ':vcs_info:*' nvcsformats "%~"
+
       # If this is an xterm set the title to user@host:dir
       case "$TERM" in
       xterm*|rxvt*|Eterm|aterm|kterm|gnome*|alacritty)
@@ -52,11 +61,36 @@
       precmd() {
         # Print the previously configured title
         print -Pnr -- "$TERM_TITLE"
+
+        vcs_info
+      }
+
+      # print git branch/rev using prompt formatting
+      git-branch ()
+      {
+        {
+          git symbolic-ref -q HEAD
+        } &> /dev/null
+
+        # 0: normal
+        # 1: detached HEAD
+        # other: no git repo
+        STATUS=$?
+
+        if [ $STATUS -eq 0 ]; then
+          BRANCH="%F{14}$(git branch --show-current)"
+        elif [ $STATUS -eq 1 ]; then
+          BRANCH="%F{red}$(git rev-parse --short HEAD)"
+        else
+          return
+        fi
+
+        echo "%F{green}(%B$BRANCH%b%F{green})-%f"
       }
 
       VI_MODE="blue"
       prompt_symbol=" "
-      PROMPT=$'%F{green}┌──(%B%F{${promptColor}}%n%F{cyan}$prompt_symbol%F{${promptColor}}%m%b%F{green})-[%B%F{reset}%~%b%F{green}]\n└─%B%F{$VI_MODE}$%b%F{reset} '
+      PROMPT=$'%F{green}┌──(%B%F{${promptColor}}%n%F{cyan}$prompt_symbol%F{${promptColor}}%m%b%F{green})-$(git-branch)%F{green}[%B%f''${vcs_info_msg_0_}%b%F{green}]\n└─%B%F{$VI_MODE}$%b%f '
 
       # start shell in vi mode
       bindkey -v
