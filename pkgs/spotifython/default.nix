7043fbeec8a43845c040b9005e4d6603f0ee1351
vawvaw <valentin@wiedekind1.de>
2024-03-29 06:24:18 +0100

    pkgs/spotifython: Update

--- pkgs/spotifython/default.nix
+++ pkgs/spotifython/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "spotifython";
-  version = "0.2.5";
+  version = "0.2.6";
 
   disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a47a0a165440caa930a78deae711fdc047513374d9fb487af080e6215e1559eb";
+    sha256 = "7d31b5306b9c1de69469b07e1f56f99d48f06ad5519702125f85b37b3a8674c7";
   };
 
   propagatedBuildInputs = [ requests ];
