21c0ad05009645f92e214ded7260702bddc113d5
vaw <valentin@wiedekind1.de>
2024-05-01 11:12:47 +0200

    freesweep: Add patch for single handed use

--- overlays/freesweep/default.nix
+++ overlays/freesweep/default.nix
@@ -1,5 +1,6 @@
 _final: prev: {
   freesweep = prev.freesweep.overrideAttrs (o: {
-    patches = (o.patches or [ ]) ++ [ ./chars.patch ./xkg_dirs.patch ];
+    patches = (o.patches or [ ])
+      ++ [ ./chars.patch ./flag.patch ./xkg_dirs.patch ];
   });
 }
