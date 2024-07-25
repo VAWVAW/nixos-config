ec44fca011c8562e252b3cacc3c38912b444ff93
vaw <valentin@wiedekind1.de>
2024-07-25 14:29:58 +0200

    shells/hosts: Process arguments

--- shells/hosts.nix
+++ shells/hosts.nix
@@ -4,22 +4,22 @@ pkgs.mkShell {
     let
       nbuild = hostName:
         (writeShellScriptBin "nbuild-${hostName}"
-          "nixos-rebuild build --flake .#${hostName}");
+          "nixos-rebuild build --flake .#${hostName} $@");
       ntest = hostName:
         (writeShellScriptBin "ntest-${hostName}"
-          "nixos-rebuild test --flake .#${hostName} --use-remote-sudo --target-host ${hostName}");
+          "nixos-rebuild test --flake .#${hostName} --use-remote-sudo --target-host ${hostName} $@");
       nswitch = hostName:
         (writeShellScriptBin "nswitch-${hostName}"
-          "nixos-rebuild switch --flake .#${hostName} --use-remote-sudo --target-host ${hostName}");
+          "nixos-rebuild switch --flake .#${hostName} --use-remote-sudo --target-host ${hostName} $@");
       nboot = hostName:
         (writeShellScriptBin "nboot-${hostName}"
-          "nixos-rebuild boot --flake .#${hostName} --use-remote-sudo --target-host ${hostName}");
+          "nixos-rebuild boot --flake .#${hostName} --use-remote-sudo --target-host ${hostName} $@");
     in [
-      (writeShellScriptBin "nbuild" "nixos-rebuild build --flake .#")
-      (writeShellScriptBin "ntest" "sudo nixos-rebuild test --flake .#")
-      (writeShellScriptBin "nswitch" "sudo nixos-rebuild switch --flake .#")
-      (writeShellScriptBin "nboot" "sudo nixos-rebuild boot --flake .#")
-      (writeShellScriptBin "hswitch" "home-manager switch --flake .")
+      (writeShellScriptBin "nbuild" "nixos-rebuild build --flake .# $@")
+      (writeShellScriptBin "ntest" "sudo nixos-rebuild test --flake .# $@")
+      (writeShellScriptBin "nswitch" "sudo nixos-rebuild switch --flake .# $@")
+      (writeShellScriptBin "nboot" "sudo nixos-rebuild boot --flake .# $@")
+      (writeShellScriptBin "hswitch" "home-manager switch --flake . $@")
 
       (nbuild "artemis")
       (ntest "artemis")
