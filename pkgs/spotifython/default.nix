6d4913a00d12605fa78ccbc23df9b31323433d51
vaw <valentin@wiedekind1.de>
2024-08-27 13:52:52 +0200

    pkgs/spotifython: Update to 0.2.9

--- pkgs/spotifython/default.nix
+++ pkgs/spotifython/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "spotifython";
-  version = "0.2.8";
+  version = "0.2.9";
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.10";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2fe47ea908df4085df69cc4db85ac6535af283968c3165d528302db8acc9fdff";
+    sha256 = "7225e763377656fe34962a0e1e4fae0fad0893158705da60877142c0dbc68820";
   };
 
   propagatedBuildInputs = [ requests ];
