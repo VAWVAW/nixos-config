58a1ed629289d117d9c74b429a8b2b46dacb909b
vaw <valentin@wiedekind1.de>
2024-09-13 22:33:02 +0200

    shells: Update lua,nix

--- shells/nix.nix
+++ shells/nix.nix
@@ -1,2 +1,2 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell { buildInputs = with pkgs; [ nil nixfmt-classic statix ]; }
+pkgs.mkShell { buildInputs = with pkgs; [ nil nixfmt-classic statix deadnix ]; }
