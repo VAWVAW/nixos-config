d9932c32d99b6589d5fd7f76136ad8787b3fb86d
vaw <valentin@wiedekind1.de>
2024-10-21 20:44:11 +0200

    nixvim/languages/c: Add gdb dap configuration

--- nixvim/languages/c.nix
+++ nixvim/languages/c.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: {
+{ config, pkgs, lib, helpers, ... }: {
   options.languages.c.enable = lib.mkEnableOption "c language support";
 
   config.plugins = lib.mkIf config.languages.c.enable {
@@ -7,5 +7,21 @@
 
     dap.enabled = true;
     dap.extensions.dap-lldb.enable = true;
+
+    dap.adapters.executables."gdb" = {
+      command = "${pkgs.gdb}/bin/gdb";
+      args = [ "--interpreter=dap" "--eval-command" "set print pretty on" ];
+    };
+    dap.extensions.dap-lldb.customConfigurations."c" = [{
+      name = "Debug (gdb)";
+      type = "gdb";
+      request = "launch";
+      cwd = "\${workspaceFolder}";
+      program = helpers.mkRaw ''
+        function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end
+      '';
+    }];
   };
 }
