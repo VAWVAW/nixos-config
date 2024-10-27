a3ebaa70a1ea9ba3f525ae58e77dd72eae3c1ff6
vaw <valentin@wiedekind1.de>
2024-10-27 11:17:00 +0100

    hades: Remove ovmf specialisation

--- hosts/hades/config/default.nix
+++ hosts/hades/config/default.nix
@@ -1,6 +1,5 @@
 {
-  imports =
-    [ ./arduino.nix ./binary-cache.nix ./network.nix ./nvidia.nix ./ovmf.nix ];
+  imports = [ ./arduino.nix ./binary-cache.nix ./network.nix ./nvidia.nix ];
   programs.adb.enable = true;
   services.ratbagd.enable = true;
 }
