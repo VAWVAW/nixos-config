3c12d3a4774ba5ab2b416a1262b7a1392cd50b08
vaw <valentin@wiedekind1.de>
2024-11-07 14:26:36 +0100

    hosts/nyx: Serve dns requests from initrd

--- hosts/nyx/config/network.nix
+++ hosts/nyx/config/network.nix
@@ -1,4 +1,5 @@
-{
+let ipv4 = "192.168.2.20";
+in {
   networking.useDHCP = false;
 
   systemd.network = {
@@ -7,9 +8,19 @@
 
     networks."40-enu1u1u1" = {
       matchConfig.Name = "enu1u1u1";
-      address = [ "192.168.2.20/24" ];
+      address = [ "${ipv4}/24" ];
       routes = [{ Gateway = "192.168.2.1"; }];
       linkConfig.RequiredForOnline = "routable";
     };
   };
+
+  # enable resolved in initrd
+  boot.initrd.systemd.contents."/etc/systemd/resolved.conf.d/override.conf".text =
+    ''
+      [Resolve]
+      DNS=1.1.1.1
+      FallbackDNS=8.8.8.8
+
+      DNSStubListenerExtra=${ipv4}
+    '';
 }
