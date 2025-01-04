b55ad27c5fc77ded7bb339643a5fea6b6a33bfdf
vaw <valentin@wiedekind1.de>
2025-01-04 18:03:33 +0100

    host/hades: Reduce configuration limit

--- hosts/hades/default.nix
+++ hosts/hades/default.nix
@@ -46,7 +46,7 @@
         efiSupport = true;
         device = "nodev";
         splashImage = null;
-        configurationLimit = 30;
+        configurationLimit = 25;
       };
     };
     binfmt.emulatedSystems = [ "aarch64-linux" ];
