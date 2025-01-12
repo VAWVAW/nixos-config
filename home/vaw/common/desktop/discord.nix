e28bbc55197d664bbafe15480d689c3777e1b971
vaw <valentin@wiedekind1.de>
2025-01-12 18:25:59 +0100

    home/discord: Support opening links

--- home/vaw/common/desktop/discord.nix
+++ home/vaw/common/desktop/discord.nix
@@ -9,8 +9,20 @@ in {
       method = "bindfs";
     }];
 
-    xdg.configFile."discord/settings.json".text =
-      ''{ "SKIP_HOST_UPDATE": true }'';
+    xdg = {
+      configFile = {
+        "discord/settings.json".text = ''{ "SKIP_HOST_UPDATE": true }'';
+        "firejail/discord.local".text = "ignore join-or-start";
+      };
+      desktopEntries."discord" = {
+        type = "Application";
+        name = "Discord";
+        categories = [ "Network" "InstantMessaging" ];
+        exec = "discord --url -- %u";
+        mimeType = [ "x-scheme-handler/discord" ];
+        icon = "discord";
+      };
+    };
 
     home.packages = let
       discord = (outputs.lib.wrapFirejailBinary {
@@ -19,11 +31,14 @@ in {
         wrappedExecutable =
           "${pkgs.discord.override { nss = pkgs.nss_latest; }}/bin/discord";
         profile = "${pkgs.firejail}/etc/firejail/discord.profile";
-        extraArgs = [ "--dbus-user.talk=org.freedesktop.Notifications" ];
+        extraArgs = [
+          "--dbus-user.talk=org.freedesktop.Notifications"
+          "--ignore=private-tmp"
+        ];
       });
     in [
       (pkgs.writeShellScriptBin "discord" ''
-        NIXOS_OZONE_WL= ${pkgs.expect}/bin/unbuffer ${discord}/bin/discord
+        NIXOS_OZONE_WL= ${pkgs.expect}/bin/unbuffer ${discord}/bin/discord "$@"
       '')
     ];
   };
